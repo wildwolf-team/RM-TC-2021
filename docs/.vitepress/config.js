1cffb0ad8894f6c7c1c1db77134a8c7ee45f83e0
iChizer0 <62390647+iChizer0@users.noreply.github.com>
2021-02-21 19:40:30 +0800

    Create vitepress config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: 'GCU Robot',
+    description: 'GCU Robot Intra-team Competition Documention',
+
+    themeConfig: {
+        repo: 'iChizer0/RM-TC-2021',
+        docsDir: 'docs',
+        docsBranch: 'main',
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑',
+        lastUpdated: '最后更新'
+    }
+}
